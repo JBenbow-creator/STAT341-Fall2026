29d1c375ce6688943a56eaeddd82d8dcca3dbaf6
JBenbow-creator <jbenbow@claflin.edu>
2026-08-19 11:07:52 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,11 @@
+#Name: Benbow, Date: 08/18/2026, Purpose: To test functions of descriptive statistics
+
+# Create a variable called variable1 and store 5 numbers from 1-5
+
+variable1 <- c(1,2,3,4,5)
+
+# Calculate mean of variable1
+
+mean(variable1)
+
+
